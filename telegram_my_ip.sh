d409afddf72d5f60674128bea737510746161ecf
DLx <71771541+danielelussana@users.noreply.github.com>
2025-08-14 15:12:33 +0200

    Update telegram_my_ip.sh
    corretto errore: era un duplicato della v1

--- telegram_my_ip.sh
+++ telegram_my_ip.sh
@@ -15,22 +15,30 @@
 #then:
 #sudo chmod 644 /etc/systemd/system/telegram_my_ip.service
 #sudo systemctl enable telegram_my_ip.service
-#Alternative method to run at boot:
+#Alternative method to run at boot hero:
 #https://www.dexterindustries.com/howto/auto-run-python-programs-on-the-raspberry-pi/
 
 #VARIABLES
-
 telegram_bot_api="YOUR_BOT_TOKEN_HERE"
 telegram_chat="YOUR_CHAT_ID_HERE"
-myip=$(hostname -I | awk '{print $1}')
-message="$(hostname) is running with IP address: $myip"
+
+# Metodo 1: Filtra solo IPv4 da hostname -I
+myip=$(hostname -I | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | head -1)
+
+# Metodo 2 (alternativo): Usa ip route per ottenere l'IP della route di default
+# myip=$(ip route get 8.8.8.8 | grep -oP 'src \K\S+')
+
+# Metodo 3 (alternativo): Specifica un'interfaccia particolare (es. eth0 o wlan0)
+# myip=$(ip addr show eth0 | grep -oP 'inet \K[\d.]+')
+# myip=$(ip addr show wlan0 | grep -oP 'inet \K[\d.]+')
+
+# Verifica che abbiamo ottenuto un IP valido
+if [[ -z "$myip" ]]; then
+    message="$(hostname) è avviato ma non è stato possibile determinare l'indirizzo IPv4"
+else
+    message="$(hostname) is running with IP address: $myip"
+fi
 
 curl -s -X POST "https://api.telegram.org/bot${telegram_bot_api}/sendMessage" \
      -d "chat_id=${telegram_chat}" \
      -d "text=${message}"
-
-
-#myip=`echo $(hostname -I | sed 's| |%20|g')`
-#message="$HOSTNAME completed reboot and has a new local IP address: $myip"
-
-#curl "https://api.telegram.org/bot$telegram_bot_api/sendMessage?chat_id=$telegram_chat&text=$message"
